8b5ca137839299ee1ea9ad0626284ad694a245e7
Surya-8894 <94678126+Surya-8894@users.noreply.github.com>
2022-01-19 22:38:28 +0530

    Update p8final.c

--- p8final.c
+++ p8final.c
@@ -1,15 +1,13 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 int main()
 {
-  int n;
-  double v;
-
-  printf("enter a number: ");
-  scanf("%d",&n);
-
-  v = sqrt(n);
-
-  printf("square root of %d is %f",n,v);
-
+  
+  float n , squareroot;
+  printf("Enter the number:");
+  scanf("%f",&n);
+  squareroot = sqrt(n);
+  printf("square root of %f is %f",n,squareroot);
+  return 0;
+  
 }
